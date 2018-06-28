7a47f387356dc047a18d8d439135bd5a4946d1e3
Ally Palanzi <ampalanzi@gmail.com>
2018-06-28 19:11:53 -0400

    add services tests

--- src/services.js
+++ src/services.js
@@ -2,9 +2,10 @@ export async function getBooks() {
   try {
     let response = await fetch('https://www.googleapis.com/books/v1/volumes?q=subject%3Ascience&maxResults=20');
     let data = await response.json();
+    // TODO: error handling if items doesn't exist for some reason
     return data.items;
   } catch(err) {
-    new Error('Error fetching books');
+    throw new Error('Error fetching books');
   }
 };
 
@@ -12,8 +13,10 @@ export async function getBook(id) {
   try {
     let response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=id%3A${id}`);
     let data = await response.json();
+    // The single book is returned as an array within items, so lets just get the First
+    // TODO: error handling if there aren't any lol
     return data.items[0];
   } catch(err) {
-    new Error(`Error fetching book with ID ${id}`);
+    throw new Error(`Error fetching book with ID ${id}`);
   }
 };
--- src/services.test.js
+++ src/services.test.js
@@ -1,5 +1,53 @@
-describe('services', () => {
-  it.skip('calls fetch', () => {
-    
+import { getBooks, getBook } from './services';
+ 
+describe('getBooks', () => {
+  it('returns items from successful call', async function () {
+    global.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => { return { items: 'some books' } }
+      })
+    );
+    const response = await getBooks();
+    expect(response).toBe('some books');
+  });
+  
+  it('returns an error from an unsuccessful call', async function () {
+    global.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: false,
+      })
+    );
+    try {
+      const response = await getBooks();
+    } catch (err) {
+      expect(err).toEqual(new Error('Error fetching books'));
+    }
+  });
+});
+
+describe('getBook', () => {
+  it('returns a book from successful call', async function () {
+    global.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => { return { items: ['some book'] } }
+      })
+    );
+    const response = await getBook(12);
+    expect(response).toBe('some book');
+  });
+  
+  it('returns an error from an unsuccessful call', async function () {
+    global.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: false,
+      })
+    );
+    try {
+      const response = await getBook(5);
+    } catch (err) {
+      expect(err).toEqual(new Error('Error fetching book with ID 5'));
+    }
   });
 });
