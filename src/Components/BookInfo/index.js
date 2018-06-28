581df7e5db9fe8b853bb61338cc692cfed85a911
Ally Palanzi <ampalanzi@gmail.com>
2018-06-28 08:46:42 -0400

    styling

--- src/Components/BookInfo/index.js
+++ src/Components/BookInfo/index.js
@@ -44,7 +44,7 @@ class BookInfo extends Component {
     } else if (book) {
       return (
         <section className="c-book-info">
-          <Link to="/"><span role="img" aria-label="back icon">⬅️</span> <span className="p-link">Back</span></Link>
+          <Link className="c-book-info__link" to="/"><svg className="p-svg" viewBox="0 0 448 512"><path d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"></path></svg> <span className="p-link">Back</span></Link>
           <div className="c-book-info__container">
             <div className="c-book-info__image">
               <img src={book.volumeInfo.imageLinks.thumbnail} alt={`${book.volumeInfo.title} cover`}/>
